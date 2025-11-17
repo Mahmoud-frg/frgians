8840f7206a867ced27cb2cb19932794645ee6c0a
mga_frg <mahmoudgamal.frg@gmail.com>
2025-11-17 16:35:57 +0200

    Fix lock file sync

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,24 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require("nativewind/metro");
+
+// const config = getDefaultConfig(__dirname);
+
+// module.exports = withNativeWind(config, { input: "./app/globals.css" });
+
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./app/globals.css" });
+// Fix for expo-router entry issue on iOS
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  'expo-router/entry': path.resolve(
+    __dirname,
+    'node_modules/expo-router/entry-classic.js'
+  ),
+};
+
+// Wrap with NativeWind
+module.exports = withNativeWind(config, { input: './app/globals.css' });
