171e46c4e6e6151b16d1736a18166ba0d672b8fa
mga_frg <mahmoudgamal.frg@gmail.com>
2025-11-30 13:36:04 +0200

    last updated

--- metro.config.js
+++ metro.config.js
@@ -4,3 +4,20 @@ const { withNativeWind } = require('nativewind/metro');
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: './app/globals.css' });
+
+// const { getDefaultConfig } = require('expo/metro-config');
+// const { withNativeWind } = require('nativewind/metro');
+// const path = require('path');
+
+// const config = getDefaultConfig(__dirname);
+
+// // Alias react-native-worklets to ensure only one copy is used
+// config.resolver.extraNodeModules = {
+//   ...(config.resolver.extraNodeModules || {}),
+//   'react-native-worklets': path.resolve(
+//     __dirname,
+//     'node_modules/react-native-worklets'
+//   ),
+// };
+
+// module.exports = withNativeWind(config, { input: './app/globals.css' });
